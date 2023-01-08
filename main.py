2c06c32e414088a9c7528c02839a35da3272b59f
Cancerous115 <107626947+Cancerous115@users.noreply.github.com>
2023-01-08 20:43:59 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,12 @@
+print("=== This is Your Story!===")
+print(" Welcome to The World of Frazzo!","I will be asking you many questions about you adventurer and what abilities you wish to obtain on this quest.")
+print()
+print("So let's get started.")
+yourName =input("What is your name adventurer?")
+yourPowers=input(" What powers do you wish to seek?")
+print("\033[31m", "Do you travel with a companion?","\033[0m")
+yourCompanion=input("What is your companions name if any?")
+yourHome=input("Where do you originally hail from?")
+print()
+
+print(" Hello", yourName," Welcome to the world of Frazzo! Here you are seeking the powers of", yourPowers, "and travel with your companion",yourCompanion,"You come from the lands far north in","\033[31m",yourHome,"just to make all of your dreams an desires come")
